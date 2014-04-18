3263d22b13d5d2ff9f4f48bd866447ba1b586631
Hamdan Javeed <hamdan@hamdanjaveed.com>
2014-04-18 04:51:57 -0400

    Added boundaries

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public Transform bullet;
+
+	private float speed = 2500f;
+
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		float dx = Input.GetAxisRaw ("Horizontal") * speed * Time.deltaTime;
+		float dy = Input.GetAxisRaw ("Vertical") * speed * Time.deltaTime;
+
+		rigidbody2D.AddForce (new Vector2 (dx, dy));
+
+		if (Input.GetKey(KeyCode.Slash)) {
+			Instantiate(bullet, transform.position, Quaternion.identity);
+		}
 	}
+
 }
